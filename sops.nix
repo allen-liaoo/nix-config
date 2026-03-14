90ea82ffaa60fd3209d6ff647ad15cc57390dd5c
Allen Liao <wcliaw610@gmail.com>
2026-03-13 22:35:57 -0500

    fix secrets structure and auto gen age keys

--- sops.nix
+++ sops.nix
@@ -6,6 +6,11 @@
     age.sshKeyPaths = [
       # USE STRINGS, DONT NOT USE PATHS (otherwise it gets written to nix store unencrypted)
       "/etc/ssh/ssh_host_ed25519_key" # see host services.openssh config
+      
+      # sops can generate priv age key based on the pub/priv ssh key
+      # so this host may edit secrets
+      keyFile = "/var/lib/sops-nix/key.txt";
+      generateKey = true;
     ];
     # age.keyFile = "/persist/sops/age/keys.txt";
 
