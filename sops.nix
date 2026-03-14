df7c76ff4f1317cb4381f02462cc0d09eba2abe7
Allen Liao <wcliaw610@gmail.com>
2026-03-13 22:02:48 -0500

    shhhhhhh.... secrets!

--- sops.nix
+++ sops.nix
@@ -1,21 +1,21 @@
 {...}: {
   sops = {
-    defaultSopsFile = ./.sops.yaml;
+    defaultSopsFile = ./secrets/secrets.yaml;
     validateSopsFiles = true;
     
     age.sshKeyPaths = [
+      # USE STRINGS, DONT NOT USE PATHS (otherwise it gets written to nix store unencrypted)
       "/etc/ssh/ssh_host_ed25519_key" # see host services.openssh config
     ];
     # age.keyFile = "/persist/sops/age/keys.txt";
 
     secrets = {
-      # "github_deploy_key" = {
-      #   sopsFile = ../../secrets/github-deploy-key.yaml;
-      #   owner = "root";
-      #   group = "root";
-      #   mode = "0400";
-      #   neededForUsers = true;
-      # };
+      "github_ssh_key" = {
+        owner = "root";
+        group = "root";
+        mode = "0400";
+        neededForUsers = true;
+      };
     };
   };
 }
