da38b909d3e7d4b71370dbfcf2339336d12fe75e
Allen Liao <wcliaw610@gmail.com>
2026-03-26 20:04:32 -0500

    refactor(services): remove guinea

--- host/_modules/services/default.nix
+++ host/_modules/services/default.nix
@@ -2,7 +2,6 @@
 
 # Only run services on guinea
 lib.optionalAttrs (builtins.elem aln.ctx.host.name [
-  aln.inventory.hosts.guinea.name
   aln.inventory.hosts.barrybenson.name
 ]) {
   imports = aln.lib.listDirFiles ./. ++ aln.lib.listSubdirs ./.;
