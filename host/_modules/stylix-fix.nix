86b7a3153b92611e99ea97ad800341c6dfe39375
Allen Liao <wcliaw610@gmail.com>
2026-04-04 23:57:29 -0500

    fix: unconditional stylix fix for nongtk

--- host/_modules/stylix-fix.nix
+++ host/_modules/stylix-fix.nix
@@ -1,6 +1,6 @@
 { lib, aln, ... }:
 
-lib.optionalAttrs (aln.ctx.host.is.gui) {
+{
   # Stylix with gtk needs dconf enabled
   # https://github.com/nix-community/stylix/issues/139
   programs.dconf.enable = true;
