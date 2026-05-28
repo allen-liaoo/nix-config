a6685b1e4bd5f424d508cc5c9dccc500be579eed
Allen Liao <wcliaw610@gmail.com>
2026-05-27 22:05:02 -0500

    feat(mc): update players list

--- host/barrybenson/selfhosted/minecraft/players.nix
+++ host/barrybenson/selfhosted/minecraft/players.nix
@@ -1,5 +1,8 @@
 {
+  aeggzzy = "1230cf0e-31eb-4b2e-95ff-05c866738e0d";
   ayyllien = "a5afe481-53f4-4f60-8d6f-7eaba4645cfb";
   Dsnustad = "a0798cea-df6e-4b51-80a3-9c36ed5b8a22";
+  lunch2549 = "5d861f20-aef8-403e-915f-8e012be5f5a0";
   IFoundLeftClick = "d47d9e04-0867-49ca-9423-0fc204c07300";
+  Pencilman531 = "7683f775-76af-4627-a4b2-b8f9f985cd91";
 }
