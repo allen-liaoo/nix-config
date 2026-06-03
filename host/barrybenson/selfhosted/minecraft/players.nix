7d2652b575991bdca453ddae6c1943877c818003
Allen Liao <wcliaw610@gmail.com>
2026-06-03 14:16:37 -0500

    add mc player: ethan

--- host/barrybenson/selfhosted/minecraft/players.nix
+++ host/barrybenson/selfhosted/minecraft/players.nix
@@ -2,6 +2,7 @@
   aeggzzy = "1230cf0e-31eb-4b2e-95ff-05c866738e0d";
   ayyllien = "a5afe481-53f4-4f60-8d6f-7eaba4645cfb";
   Dsnustad = "a0798cea-df6e-4b51-80a3-9c36ed5b8a22";
+  ethan_not_dababy = "d145b3a4-e79a-41b4-abf6-477fa9a9162c";
   lunch2549 = "5d861f20-aef8-403e-915f-8e012be5f5a0";
   IFoundLeftClick = "d47d9e04-0867-49ca-9423-0fc204c07300";
   Pencilman531 = "7683f775-76af-4627-a4b2-b8f9f985cd91";
