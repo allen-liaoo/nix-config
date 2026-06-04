dde9c00aa433ead3dbe89e176910cda793f045fd
Allen Liao <wcliaw610@gmail.com>
2026-06-03 22:43:27 -0500

    fix(de/fonts): remove unused fonts, set default emoji font

--- home/_modules/de/font.nix
+++ home/_modules/de/font.nix
@@ -1,13 +1,13 @@
-{ lib, pkgs, ... }:
+{
+  pkgs,
+  ...
+}:
 
 {
   home.packages = with pkgs; [
-    nerd-fonts.commit-mono
-    nerd-fonts.fira-code
-
-    # system fonts
-    adwaita-fonts
     dejavu_fonts
+    adwaita-fonts
+    nerd-fonts.commit-mono
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
     noto-fonts-color-emoji
@@ -28,9 +28,11 @@
       ];
       monospace = [
         "CommitMono Nerd Font Mono"
-        "FiraCode Nerd Font Mono"
         "Noto Sans Mono CJK TC"
         "Noto Sans Mono CJK SC"
+      ];
+      emoji = [
+        "Noto Color Emoji"
       ];
     };
   };
