6c7d4396169479925c74394c356c72de8511651a
Allen Liao <wcliaw610@gmail.com>
2026-06-04 23:05:37 -0500

    fix(bitwarden-desktop): allow EOL electron dep

--- home/_modules/app/packages.nix
+++ home/_modules/app/packages.nix
@@ -8,4 +8,17 @@
     signal-desktop
     zotero
   ];
+
+  # fix dep electron-39.8.10 insecure and EOL
+  # https://github.com/NixOS/nixpkgs/issues/526914
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-39.8.10"
+  ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      bitwarden-desktop = prev.bitwarden-desktop.override {
+        electron_39 = final.electron_39-bin;
+      };
+    })
+  ];
 }
